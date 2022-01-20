f09e21eda47c6e1754d2339cd1990bb86576dd2b
Kunn <kbkhedkar2200@gmail.com>
2022-01-20 19:39:30 +0530

    Added Crouch mechanism and jump animation
    1. After clicking crouch animation starts and collider size reduces and after stopping animation collider size increases
    2. Jump animation

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -6,9 +6,13 @@ public class PlayerCtrl : MonoBehaviour
 {
     public Animator animator;
 
+
+    private Vector2 scale; 
+
     // Start is called before the first frame update
     void Start()
     {
+        scale = gameObject.GetComponent<Collider2D>().transform.localScale;
         
     }
 
@@ -18,7 +22,7 @@ public class PlayerCtrl : MonoBehaviour
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed",Mathf.Abs(speed));
 
-        Vector3 scale = transform.localScale;
+        Vector2 scale = transform.localScale;
 
         if (speed < 0)
         {
@@ -28,5 +32,39 @@ public class PlayerCtrl : MonoBehaviour
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+
+        float jump1 = Input.GetAxisRaw("Vertical");
+        if(jump1 > 0)
+        {
+            animator.SetBool("jump", true);
+        }else if(jump1 < 0)
+        {
+            animator.SetBool("jump", false);
+        }
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+
+           gameObject.GetComponent<BoxCollider2D>().size = new Vector2(0.53f, 1.29f);
+           gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(0.02f, 0.59f);
+
+            animator.SetBool("Crouch", true);
+
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+           gameObject.GetComponent<BoxCollider2D>().size = new Vector2(0.53f, 2.01f);
+           gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(0.02f, 0.95f);
+
+           animator.SetBool("Crouch", false);
+        }
     }
 }
