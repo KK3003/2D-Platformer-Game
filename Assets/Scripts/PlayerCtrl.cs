61175a4403f153ebf9280e204f40db1ed2dbb322
Kunn <kbkhedkar2200@gmail.com>
2022-01-23 00:09:21 +0530

    Added player movements
    Added player movement and jump

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -6,65 +6,92 @@ public class PlayerCtrl : MonoBehaviour
 {
     public Animator animator;
 
+    public float speed;   // speed at which player will move
+    public float jump;    // force on player jump
 
-    private Vector2 scale; 
+    private Rigidbody2D rb2D;
+
+    private void Awake()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        scale = gameObject.GetComponent<Collider2D>().transform.localScale;
+       
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed",Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
 
-        Vector2 scale = transform.localScale;
-
-        if (speed < 0)
-        {
-            scale.x = -1f * Mathf.Abs(scale.x);    
-        } else if(speed > 0)
-        {
-            scale.x = Mathf.Abs(scale.x);
-        }
-        transform.localScale = scale;
-
-
-        float jump1 = Input.GetAxisRaw("Vertical");
-        if(jump1 > 0)
-        {
-            animator.SetBool("jump", true);
-        }else if(jump1 < 0)
-        {
-            animator.SetBool("jump", false);
-        }
-
-
-    }
-
-
-    private void FixedUpdate()
-    {
+        moveCharacter(horizontal, vertical);
+        playMovementAnimation(horizontal, vertical);
+       
 
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
 
-           gameObject.GetComponent<BoxCollider2D>().size = new Vector2(0.53f, 1.29f);
-           gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(0.02f, 0.59f);
+            gameObject.GetComponent<BoxCollider2D>().size = new Vector2(0.53f, 1.29f);
+            gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(0.02f, 0.59f);
 
             animator.SetBool("Crouch", true);
 
         }
         else if (Input.GetKeyUp(KeyCode.LeftControl))
         {
-           gameObject.GetComponent<BoxCollider2D>().size = new Vector2(0.53f, 2.01f);
-           gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(0.02f, 0.95f);
+            gameObject.GetComponent<BoxCollider2D>().size = new Vector2(0.53f, 2.01f);
+            gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(0.02f, 0.95f);
 
-           animator.SetBool("Crouch", false);
+            animator.SetBool("Crouch", false);
+        }
+    }
+
+
+    void moveCharacter(float horizontal, float vertical)   // function to move player 
+    {
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+
+        if(vertical > 0)
+        {
+            rb2D.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
+        }
+    }
+
+    private void playMovementAnimation(float horizontal, float vertical)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        Vector3 scale = transform.localScale;
+
+        if (horizontal < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (horizontal > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+
+        // Jump logic
+        if(vertical > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
         }
     }
 }
+
+
+     
+
